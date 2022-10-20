97766b43cd61edf50b32072d171169aab3bfb9cd
Lucas Glave <lucasglave7@gmail.com>
2022-10-20 20:42:18 -0300

    hola

---
+++ estilos/js/main.js
@@ -0,0 +1,19 @@
+resultado = 0;
+
+
+let numero1 = parseInt(prompt("Ingresa un numero"))
+let numero2 = parseInt(prompt("ingresa otro"))
+
+function sumar(numero1, numero2){
+    resultado= numero1 + numero2;
+    alert(resultado)
+}
+
+sumar(numero1, numero2)
+
+// let nombre = prompt("Ingrese su nombre")
+// if (nombre != "gaston")
+//     alert("Su nombre es " + nombre)
+// else 
+//     alert("Que es ese nombre maestro")
+    
